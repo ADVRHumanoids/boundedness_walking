f016ab0f8faeac74f993df911472855bef9e072b
Arturo Laurenzi <arturo.laurenzi@iit.it>
2018-12-11 11:52:09 +0100

    Script to setup variables

---
+++ matlab/setup_variables.m
@@ -0,0 +1,7 @@
+A = [0 1; 1 0];
+B = [0; -1];
+lipm = ss(A, B, [1 0], 0);
+Klipm = lqr(lipm, diag([1 1]), 1);
+lipm = ss([A, [0; 0]; [0 1 0]], [B; 0], [1 0 0], 0);
+lipm_d = c2d(lipm, 1.0);
+K = dlqr(lipm_d.A, lipm_d.B, diag([0 1 1]), 0.0001);
